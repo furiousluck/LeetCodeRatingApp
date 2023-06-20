b3d985b0a068d2678fe21e1b652751e1009accec
Adarsh <singhadarsh100@gmail.com>
2023-06-20 11:49:42 +0530

    modifications

--- app/src/main/java/com/example/leetcoderating/MyViewPagerAdapter1.kt
+++ app/src/main/java/com/example/leetcoderating/MyViewPagerAdapter1.kt
@@ -4,12 +4,13 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.leetcoderating.fragments.AboutMeFragment
 import com.example.leetcoderating.fragments.FriendsFragment
 import com.example.leetcoderating.fragments.HomeFragment
 
 class MyViewPagerAdapter1(fragmentManager: FragmentManager, lifecycle: Lifecycle):FragmentStateAdapter(fragmentManager,lifecycle) {
     override fun getItemCount(): Int {
-        return 2
+        return 3
     }
 
     override fun createFragment(position: Int): Fragment {
@@ -19,6 +20,9 @@ class MyViewPagerAdapter1(fragmentManager: FragmentManager, lifecycle: Lifecycle
             }
             1->{
                 FriendsFragment()
+            }
+            2->{
+                AboutMeFragment()
             }
             else->{
                 HomeFragment()
